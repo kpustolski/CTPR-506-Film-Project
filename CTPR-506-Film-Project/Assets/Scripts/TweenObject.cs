158abad553ad84a2fbed6228416d882d5647883f
Katie Pustolski <pustolskik@gmail.com>
2016-10-26 00:18:29 -0700

    Added some packs (housing, buildings, street pack) that may come in handy. Also made a few scripts for changing the scene and making an elevator work. Worked on blocking Parts 3 and 4.

---
+++ CTPR-506-Film-Project/Assets/Scripts/TweenObject.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TweenObject : MonoBehaviour {
+
+	/// <summary>
+	/// Place on object you would like to tween.
+	/// Tween object to a position. Can move it to and from a position.
+	/// </summary>
+	//public GameObject movingObj;
+	public GameObject endPosObj;
+	public float tweenTime =1f;
+
+	private Vector3 _endPos;
+	private Vector3 _startPos;
+	// Use this for initialization
+	void Start () {
+		_endPos = endPosObj.GetComponent<Transform> ().position;
+		_startPos = this.transform.position;
+	}
+
+	public void MoveObject(){
+		iTween.MoveTo (this.gameObject,_endPos,tweenTime);
+	}
+
+	public void ResetObjPos(){
+		iTween.MoveTo (this.gameObject,_startPos,tweenTime);
+	}
+
+}
