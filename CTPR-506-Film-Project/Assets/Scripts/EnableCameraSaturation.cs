c82b0870e863bcdf90dcfd6d84ce327352de3811
Katie Pustolski <pustolskik@gmail.com>
2016-11-13 14:48:41 -0800

    Worked on changing saturation when entering the apartment.

---
+++ CTPR-506-Film-Project/Assets/Scripts/EnableCameraSaturation.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+public class EnableCameraSaturation : MonoBehaviour {
+
+	/// <summary>
+	/// Changes camera saturation;
+	/// </summary>
+	public Camera mainCamera;
+	public float increaseSatBy=0.01f;
+
+	public float camSatEnd = 1f;
+	float sat;
+
+	public float saturation
+	{
+		get
+		{
+			return sat;
+		}
+	}
+
+	// Use this for initialization
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other){
+
+		StartCoroutine (changeCameraSaturation (camSatEnd));
+	}
+	IEnumerator changeCameraSaturation(float camSat){
+		while (sat < 1f) {
+			sat = mainCamera.GetComponent<ColorCorrectionCurves> ().saturation;
+			sat += increaseSatBy;
+			mainCamera.GetComponent<ColorCorrectionCurves> ().saturation = sat;
+			yield return null;
+		}
+	}
+}
