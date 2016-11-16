011289c78564a09180406bc74d32d75d032e10e7
Katie Pustolski <pustolskik@gmail.com>
2016-11-16 15:47:53 -0800

    Added more details to scene 4.

---
+++ CTPR-506-Film-Project/Assets/Scripts/StrobeLight.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StrobeLight : MonoBehaviour {
+
+	//https://www.youtube.com/watch?v=DhoXfhHamxY
+	public float time;
+	public GameObject light;
+	public Texture lightOn;
+	public Texture lightOff;
+	private bool isStrobe=true;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("Flicker");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	IEnumerator Flicker()
+	{
+		while (isStrobe)
+		{
+			light.GetComponent<Light>().enabled = false;
+			GetComponent<Renderer> ().material.mainTexture = lightOff;
+			yield return new WaitForSeconds(time);
+			light.GetComponent<Light>().enabled = true;
+			GetComponent<Renderer> ().material.mainTexture = lightOn;
+			yield return new WaitForSeconds(time);
+		}
+
+	}
+}
---
+++ CTPR-506-Film-Project/Assets/Scripts/TurnOnLights.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnOnLights : MonoBehaviour {
+
+	public GameObject[] lightsArray;
+	public float waitTime;
+	private bool goOnce=false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+
+		if (!goOnce) {
+			StartCoroutine("LetThereBeLight");
+			goOnce = true;
+		}
+	}
+	IEnumerator LetThereBeLight()
+	{
+		yield return new WaitForSeconds(waitTime);
+		foreach (GameObject l in lightsArray) {
+			l.GetComponent<Light>().enabled = true;
+		}
+	}
+}
