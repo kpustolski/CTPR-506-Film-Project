bdf64532183fbff51f607f0aff167c5f4bbc3550
JBillingsley <jmb330@case.edu>
2016-11-17 13:47:01 -0800

    Fixed cars and scene2

---
+++ CTPR-506-Film-Project/Assets/Scripts/CarMover.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarMover : MonoBehaviour {
+    public bool flip = false;
+    public float travelSpeed = 5f;
+    public float totalDistance = 100f;
+
+    Vector3 startingPosition;
+	// Use this for initialization
+	void Start () {
+        startingPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (flip) {
+            if (transform.position.z > startingPosition.z - totalDistance) {
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - travelSpeed * Time.deltaTime);
+            } else {
+                transform.position = startingPosition;
+                randomizeStartSpeed();
+            }
+        } else {
+            if (transform.position.z < startingPosition.z + totalDistance) {
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + travelSpeed * Time.deltaTime);
+            } else {
+                transform.position = startingPosition;
+                randomizeStartSpeed();
+            }
+        }
+	}
+
+    void randomizeStartSpeed() {
+        travelSpeed += Random.Range(-20, 30);
+    }
+
+    public void normalizeSpeed() {
+        travelSpeed = 70f;
+    }
+}
