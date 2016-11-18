a3acf425763b4ac2dc41139d6604d5e64e695812
Atwood Deng <dengjdtsinghua@gmail.com>
2016-11-18 10:29:12 -0800

    add camera fliter and camera control

---
+++ CTPR-506-Film-Project/Assets/Scripts/Part1/CameraSequence.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class CameraSequence : MonoBehaviour {
+
+	[SerializeField] CameraPosition[] positionList;
+	[SerializeField] Camera m_camera;
+	[SerializeField] int index = 0;
+
+	void Awake()
+	{
+		if (m_camera == null)
+			m_camera = GetComponentInChildren<Camera> ();
+	}
+
+	void Update()
+	{
+		if ( Input.GetKeyDown(KeyCode.G) )
+		{
+			if (index < positionList.Length) {
+				CameraPosition next = positionList [index];
+
+				DealPosition (next);
+
+				index++;
+			}
+		}
+	}
+
+	void DealPosition( CameraPosition pos )
+	{
+		if (pos.fromTransform == TransformType.Direct) {
+			m_camera.enabled = true;
+			m_camera.transform.position = pos.transform.position;
+			m_camera.transform.rotation = pos.transform.rotation;
+		} else if (pos.fromTransform == TransformType.MoveFromMain) {
+			m_camera.enabled = false;
+			m_camera.transform.position = Camera.main.transform.position;
+			m_camera.transform.rotation = Camera.main.transform.rotation;
+
+			m_camera.enabled = true;
+
+			m_camera.transform.DOMove (pos.transform.position, pos.transformTime);
+			m_camera.transform.DORotate (pos.transform.rotation.eulerAngles, pos.transformTime);
+			
+		} else if (pos.fromTransform == TransformType.Unable) {
+
+			m_camera.transform.DOMove (Camera.main.transform.position, pos.transformTime);
+			m_camera.transform.DORotate (Camera.main.transform.rotation.eulerAngles, pos.transformTime).OnComplete(delegate() {
+				m_camera.enabled = false;	
+			});
+		} else if (pos.fromTransform == TransformType.Move) {
+			m_camera.enabled = true;
+
+			m_camera.transform.DOMove (pos.transform.position, pos.transformTime);
+			m_camera.transform.DORotate (pos.transform.rotation.eulerAngles, pos.transformTime);
+		}
+	}
+}
+
+[System.Serializable]
+public class CameraPosition
+{
+	public Transform transform;
+	public TransformType fromTransform;
+	public float transformTime;
+}
+
+public enum TransformType
+{
+	Direct,
+	Move,
+	MoveFromMain,
+	Unable,
+}
---
+++ CTPR-506-Film-Project/Assets/Scripts/Part1/EmailUI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EmailUI : MonoBehaviour {
+
+
+	[SerializeField] Image[] emailContent;
+	[SerializeField] GameObject email;
+	[SerializeField] GameObject desktop;
+
+	void Awake()
+	{
+	}
+
+	void Update()
+	{
+		if ( Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown (KeyCode.E)) {
+			desktop.SetActive (true);
+			Cursor.visible = true;
+		}
+		if ( Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown (KeyCode.F)) {
+			desktop.SetActive (false);
+			email.SetActive (false);
+			Cursor.visible = false;
+		}
+	}
+	public void OnShowEmail()
+	{
+		Debug.Log ("On Show Email");
+		email.SetActive (true);
+	}
+	public void OnEmailItem( int i )
+	{
+		Debug.Log ("On Email Item");
+		foreach (Image content in emailContent)
+			content.gameObject.SetActive (false);
+		emailContent [i].gameObject.SetActive (true);
+	}
+}
